2319e6d50910982b7f57b7fd560e40ef4cc2b16c
Carlos Garcia <carlos-gb@users.noreply.github.com>
2019-03-13 15:15:30 -0600

    ejemplo de clase y clase prueba

---
+++ dia2/Pokemon.java
@@ -0,0 +1,15 @@
+class Pokemon{
+    // variables
+    String nombre;
+    int numero;
+
+    /**
+     * comentario de 
+     * multiples lineas
+     */
+    //metodos
+    void detallePokemon(){
+        System.out.println(numero+" - "+nombre);
+    }
+
+}
---
+++ dia2/PokemonTest.java
@@ -0,0 +1,13 @@
+public class PokemonTest{
+    public static void main(String[]args){
+
+        //Crear variable
+        Pokemon charizard;
+        
+        //Inicializar variable
+        charizard=new Pokemon();
+        
+        //ejecutar metodo
+        charizard.detallePokemon();
+    }
+}
